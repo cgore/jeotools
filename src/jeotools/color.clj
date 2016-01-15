7b8bd0262115b57ff8876cdc932c8e98d054ffdb
Christopher Mark Gore <cgore@cgore.com>
2016-01-15 17:00:15 -0600

    Adding a generic color builder.

--- src/jeotools/color.clj
+++ src/jeotools/color.clj
@@ -10,3 +10,40 @@
     (into {} (map #(hash-map (keyword %)
                              (eval (symbol (str "java.awt.Color/" %))))
                   awt-color-names))))
+
+(defn color
+  "This function is intended to process *anything* that can reasonably be viewed
+  as a color and return a java.awt.Color instance for it."
+  ([rgb]
+   (cond (integer? rgb)
+         (Color. (int rgb))
+
+         (instance? Color rgb)
+         rgb
+
+         (string? rgb)
+         (Color/decode rgb)
+
+         :else
+         (Color. rgb)))
+  ([rgba has-alpha]
+   (Color. (int rgba) (boolean has-alpha)))
+  ([r g b]
+   (cond (every? integer? [r g b])
+         (Color. (int r) (int g) (int b))
+
+         (every? float? [r g b])
+         (Color. (float r) (float g) (float b))
+
+         :else
+         (Color. r g b)))
+  ([r g b a]
+   (cond (every? integer? [r g b a])
+         (Color. (int r) (int g) (int b) (int a))
+
+         (every? float? [r g b a])
+         (Color. (float r) (float g) (float b) (float a))
+
+         :else
+         (Color. r g b a))))
+
