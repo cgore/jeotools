5f112620c9a7d9d94040dde8d1e634750b6ef564
Christopher Mark Gore <cgore@cgore.com>
2016-01-15 10:37:21 -0600

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jeotools
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/jeotools/core_test.clj
@@ -0,0 +1,7 @@
+(ns jeotools.core-test
+  (:require [clojure.test :refer :all]
+            [jeotools.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
