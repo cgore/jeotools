3bafc09cbcd8b8381dcd4e6ab890b7a0c139d31c
Christopher Mark Gore <cgore@cgore.com>
2016-01-15 10:40:30 -0600

    Adding GeoTools dependencies to project.clj.

--- project.clj
+++ project.clj
@@ -3,4 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :repositories {"osgeo" "http://download.osgeo.org/webdav/geotools"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.geotools/gt-main      "14.1"]
+                 [org.geotools/gt-coverage  "14.1"]
+                 [org.geotools/gt-epsg-wkt  "14.1"]
+                 [org.geotools/gt-geojson   "14.1"]
+                 [org.geotools/gt-geotiff   "14.1"]
+                 [org.geotools/gt-image     "14.1"]
+                 [org.geotools/gt-render    "14.1"]
+                 [org.geotools/gt-shapefile "14.1"]
+                 [org.geotools/gt-wms       "14.1"]])
