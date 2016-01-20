459de0723cf3c344a27f47c931967f8197d2b476
Christopher Mark Gore <cgore@cgore.com>
2016-01-20 12:35:34 -0600

    Adding test-with-files dependency.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/cgore/jeotools"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:resource-paths ["test/resources"]}}
+  :profiles {:dev {:dependencies [[test-with-files "0.1.1"]]
+                   :resource-paths ["test/resources"]}}
   :repositories {"osgeo" "http://download.osgeo.org/webdav/geotools"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.geotools/gt-main      "14.1"]
