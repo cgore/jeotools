c472564e3e30a0ef3d9035e549d62572ba96475b
Christopher Mark Gore <cgore@cgore.com>
2016-01-24 14:02:47 -0600

    Adding all the rest of the CRS creation functions.

--- src/jeotools/referencing/crs.clj
+++ src/jeotools/referencing/crs.clj
@@ -11,7 +11,51 @@
   ([longitude-first?]
    (org.geotools.referencing.CRS/getAuthorityFactory longitude-first?)))
 
+(defn create-crs
+  "Returns an arbitrary coordinate reference system from a code."
+  (.createCoordinateReferenceSystem (get-authority-factory) code))
+
+(defn create-compound-crs
+  "Creates a 3D coordinate reference system from a code."
+  [code]
+  (.createCompoundCRS (get-authority-factory) code))
+
+(defn create-derived-crs
+  "Creates a derived coordinate reference system from a code."
+  [code]
+  (.createDerivedCRS (get-authority-factory) code))
+
+(defn create-engineering-crs
+  "Creates an engineering coordinate reference system from a code."
+  [code]
+  (.createEngineeringCRS (get-authority-factory) code))
+
+(defn create-geocentric-crs
+  "Creates a geocentric coordinate reference system from a code."
+  [code]
+  (.createGeocentricCRS (get-authority-factory) code))
+
 (defn create-geographic-crs
-  "Given a name of a geographic CRS, create a GeographicCRS object."
+  "Returns a geographic coordinate reference system from a code."
   [code]
   (.createGeographicCRS (get-authority-factory) code))
+
+(defn create-image-crs
+  "Creates an image coordinate reference system from a code."
+  [code]
+  (.createImageCRS (get-authority-factory) code))
+
+(defn create-projected-crs
+  "Creates a projected coordinate reference system from a code."
+  [code]
+  (.createProjectedCRS (get-authority-factory) code))
+
+(defn create-temporal-crs
+  "Creates a temporal coordinate reference system from a code."
+  [code]
+  (.createTemporalCRS (get-authority-factory) code))
+
+(defn create-vertical-crs
+  "Creates a vertical coordinate reference system from a code."
+  [code]
+  (.createVerticalCRS (get-authority-factory) code))
