a33b74abe9861afe5eefaf71eca85d700b2e38da
Christopher Mark Gore <cgore@cgore.com>
2016-01-24 13:52:13 -0600

    Link to javadocs.

--- src/jeotools/referencing/crs.clj
+++ src/jeotools/referencing/crs.clj
@@ -1,5 +1,9 @@
 (ns jeotools.referencing.crs)
 
+;;; org.geotools.referencing
+;;; class CRS
+;;; Javasocs: <http://docs.geotools.org/latest/javadocs/org/geotools/referencing/CRS.html>
+
 (defn create-geographic-crs
   "Given a name of a geographic CRS, create a GeographicCRS object."
   [code]
