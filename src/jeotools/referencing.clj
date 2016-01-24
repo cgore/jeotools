0718325b58ce5f4dfb9ba5eddfacb4246d4d7c3a
Christopher Mark Gore <cgore@cgore.com>
2016-01-24 13:48:45 -0600

    name -> code.

--- src/jeotools/referencing.clj
+++ src/jeotools/referencing.clj
@@ -2,5 +2,5 @@
 
 (defn create-geographic-crs
   "Given a name of a geographic CRS, create a GeographicCRS object."
-  [name]
-  (.createGeographicCRS (org.geotools.referencing.CRS/getAuthorityFactory false) name))
+  [code]
+  (.createGeographicCRS (org.geotools.referencing.CRS/getAuthorityFactory false) code))
