c2e6bb2d994c5dc50ff5b113309fed31f9219b28
Christopher Mark Gore <cgore@cgore.com>
2016-01-24 13:55:40 -0600

    Breaking out get-authority-factory.

--- src/jeotools/referencing/crs.clj
+++ src/jeotools/referencing/crs.clj
@@ -4,7 +4,14 @@
 ;;; class CRS
 ;;; Javasocs: <http://docs.geotools.org/latest/javadocs/org/geotools/referencing/CRS.html>
 
+(defn get-authority-factory
+  "Returns the CRS authority factory used by the decode methods."
+  ([]
+   (get-authority-factory false))
+  ([longitude-first?]
+   (org.geotools.referencing.CRS/getAuthorityFactory longitude-first?)))
+
 (defn create-geographic-crs
   "Given a name of a geographic CRS, create a GeographicCRS object."
   [code]
-  (.createGeographicCRS (org.geotools.referencing.CRS/getAuthorityFactory false) code))
+  (.createGeographicCRS (get-authority-factory) code))
