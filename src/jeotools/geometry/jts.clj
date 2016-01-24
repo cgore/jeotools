c1f6d71c5c58b173796285d75e23c523ad717d69
Christopher Mark Gore <cgore@cgore.com>
2016-01-24 15:44:17 -0600

    Support for CoordinateArraySequence's and LinearRing's.

--- src/jeotools/geometry/jts.clj
+++ src/jeotools/geometry/jts.clj
@@ -1,14 +1,5 @@
 (ns jeotools.geometry.jts)
 
-;;; org.geotools.geometry.jts
-;;; Javadocs: <http://docs.geotools.org/stable/javadocs/org/geotools/geometry/jts/JTS.html>
-
-;;; com.vividsolutions.jts.geom.GeometryFactory
-;;; Javadocs: <http://tsusiatsoftware.net/jts/javadoc/com/vividsolutions/jts/geom/GeometryFactory.html>
-
-(defn geometry-factory []
-  (com.vividsolutions.jts.geom.GeometryFactory.))
-
 ;;; com.vividsolutions.jts.geom.Coordinate
 ;;; Javadocs: <http://tsusiatsoftware.net/jts/javadoc/com/vividsolutions/jts/geom/Coordinate.html>
 
@@ -22,6 +13,27 @@
   ([x y z]
    (com.vividsolutions.jts.geom.Coordinate. x y z)))
 
+(defn coordinate-array [v]
+  (into-array com.vividsolutions.jts.geom.Coordinate
+              (map #(apply coordinate %) v)))
+
+(defn coordinate-array-sequence [coordinates]
+  (com.vividsolutions.jts.geom.impl.CoordinateArraySequence. coordinates))
+
+;;; com.vividsolutions.jts.geom.GeometryFactory
+;;; Javadocs: <http://tsusiatsoftware.net/jts/javadoc/com/vividsolutions/jts/geom/GeometryFactory.html>
+
+(defn geometry-factory []
+  (com.vividsolutions.jts.geom.GeometryFactory.))
+
+;;; com.vividsolutions.jts.geom.LinearRing
+;;; Javadocs: <http://tsusiatsoftware.net/jts/javadoc/com/vividsolutions/jts/geom/LinearRing.html>
+
+(defn linear-ring [coordinates]
+  (com.vividsolutions.jts.geom.LinearRing. coordinates (geometry-factory)))
+
 ;;; com.vividsolutions.jts.geom.Geometry
 ;;; Javadocs: <http://tsusiatsoftware.net/jts/javadoc/com/vividsolutions/jts/geom/Geometry.html>
 
+;;; org.geotools.geometry.jts
+;;; Javadocs: <http://docs.geotools.org/stable/javadocs/org/geotools/geometry/jts/JTS.html>
