10a39ef326bdd8f6d82f10de453481841008df84
Christopher Mark Gore <cgore@cgore.com>
2016-01-26 10:39:45 -0600

    Adding color-map-entry.

--- src/jeotools/styling.clj
+++ src/jeotools/styling.clj
@@ -1,4 +1,5 @@
-(ns jeotools.styling)
+(ns jeotools.styling
+  (:require [jeotools.filter :refer [literal-expression]]))
 
 (defn read-sld-file [url-path]
   (let [style-factory (org.geotools.factory.CommonFactoryFinder/getStyleFactory nil)
@@ -9,3 +10,9 @@
 
 (defn create-polygon-style [outline-color fill-color opacity]
   (org.geotools.styling.SLD/createPolygonStyle outline-color fill-color opacity))
+
+(defn color-map-entry [{:keys [color quantity opacity]}]
+  (doto (org.geotools.styling.ColorMapEntryImpl.)
+    (.setColor (literal-expression color))
+    (.setQuantity (literal-expression quantity))
+    (.setOpacity (literal-expression opacity))))
